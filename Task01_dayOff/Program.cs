15627497f39a81b221d1a5e6d3b333e8f27d84ef
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-31 21:25:33 +0300

    Task01 about day off added

---
+++ Task01_dayOff/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите номер дня недели от 1 до 7 включительно ");
+int dayWeekNumber = int.Parse(Console.ReadLine());
+if (dayWeekNumber>=1 && dayWeekNumber<=5)
+Console.WriteLine ("Это не выходной день");
+else if (dayWeekNumber==6 || dayWeekNumber==7)
+Console.WriteLine ("Это выходной день");
+else 
+Console.WriteLine ("Вы ввели число, которое не входит в указанный диапазон :(");
