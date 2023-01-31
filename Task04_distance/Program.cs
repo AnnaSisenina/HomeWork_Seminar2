506fcaa389df5752237dc67e4c3ad7bd16da7830
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-31 22:20:39 +0300

    Task04 abot distance between two points added

---
+++ Task04_distance/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+Console.WriteLine("Введите координаты первой точки (a,b,0)/(a,b,c)");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки (d,e,0)/(d,e,f)");
+int d = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+int f = int.Parse(Console.ReadLine());
+double distance = Math.Sqrt((d-a)*(d-a)+(e-b)*(e-b)+(f-c)*(f-c));
+Console.WriteLine("Расстояние между точками равно "+distance);
