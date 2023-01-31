0f337670d15c1e151b19c3dd572fe4454e5c758a
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-31 21:54:08 +0300

    Task 03 about quadrants and coordinates added

---
+++ Task03_coordinates/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти от 1 до 4:");
+int quarter = int.Parse(Console.ReadLine());
+if (quarter==1)
+Console.WriteLine("Диапазон возможных координат: x>0 и y>0");
+else if (quarter==2)
+Console.WriteLine("Диапазон возможных координат: x<0 и y>0");
+else if (quarter==3)
+Console.WriteLine("Диапазон возможных координат: x<0 и y<0");
+else if (quarter==4)
+Console.WriteLine("Диапазон возможных координат: x>0 и y<0");
+else
+Console.WriteLine("Вы ввели число, не соответствующее ни одной четверти");
