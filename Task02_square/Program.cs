423b827889afbecfd0b198b61524d423045e4204
Anna Sisenina <yuta4ka71@gmail.com>
2023-01-31 21:34:03 +0300

    Task02 about square number added

---
+++ Task02_square/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите первое число:");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = int.Parse(Console.ReadLine());
+if (firstNumber==secondNumber*secondNumber)
+Console.WriteLine("Первое число является квадратом второго");
+else if (secondNumber==firstNumber*firstNumber)
+Console.WriteLine("Второе число является квадратом первого");
+else
+Console.WriteLine("Ни одно из чисел не является квадратом другого");
